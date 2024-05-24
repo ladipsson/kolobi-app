d0995e2171235002e18b9a4e282087ff6aaae338
Shadrach Arowolo <princetunde17@gmail.com>
2024-05-24 19:22:24 +0100

    codes

---
+++ src/Components/Artistcontent.js
@@ -0,0 +1,47 @@
+import Aneesh from '../assets/adults/aneesh-mandava.jpg'
+import Austin from '../assets/adults/austin-neill.jpg'
+import Curtis from '../assets/adults/curtis-potvin.jpg'
+import Dushawn from '../assets/adults/dushawn-jovic.jpg'
+import Elice from '../assets/adults/elice-moore.jpg'
+import Jakayla from '../assets/adults/jakayla-toney.jpg'
+
+const artistData = [
+    {
+        id: 1,
+        name: "Aneesh Mandava",
+        image: Aneesh,
+        followers: "2.5M Followers",
+    },
+    {
+        id: 2,
+        name: "Austin Neill",
+        image: Austin,
+        followers: "6.5M Followers",
+    },
+    {
+        id: 3,
+        name: "Curtis Potvin",
+        image: Curtis,
+        followers: "2M Followers",
+    },
+    {
+        id: 4,
+        name: "Dushawn Jovic",
+        image: Dushawn,
+        followers: "10.5M Followers",
+    },
+    {
+        id: 5,
+        name: "Elice Moore",
+        image: Elice,
+        followers: "5.5M Followers",
+    },
+    {
+        id: 6,
+        name: "Jakayla Toney",
+        image: Jakayla,
+        followers: "11M Followers",
+    },
+]
+
+export default artistData;
---
+++ src/Components/Leftpanel.jsx
@@ -0,0 +1,25 @@
+
+const Leftpanel = () => {
+  return (
+    <section className=" left-panel w-1/4 flex flex-col flex-1 items-center text-center pt-7 rounded-tr-3xl bg-[--crimson] h-full">
+      <div className="top-listen">
+        <h2 className="capitalize text-[--pigmentGreen] font-semibold">Top Listen</h2>
+        <ul className="text-[--babyPowder] text-sm font-semibold">
+          <li className="my-2" >Discover</li>
+          <li className="my-2" >Kids Music</li>
+          <li className="my-2" >Adult Music</li>
+        </ul>
+      </div>
+      <div className="faves my-6">
+        <h2 className="capitalize text-[--pigmentGreen] font-semibold">Favorites</h2>
+        <ul className="text-[--babyPowder] text-sm font-semibold">
+          <li className="my-2" >Summer Vibes</li>
+          <li className="my-2" >Chill Out</li>
+          <li className="my-2" >Classic Rock</li>
+        </ul>
+      </div>
+    </section>
+  )
+}
+
+export default Leftpanel
