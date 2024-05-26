2570c9dfb021a5c0e68c2bd930ad8fb999fb0b91
Shadrach Arowolo <princetunde17@gmail.com>
2024-05-26 17:30:57 +0100

    Add files via upload

---
+++ src/Components/Search/SearchBar.jsx
@@ -0,0 +1,14 @@
+
+
+const SearchBar = () => {
+    return (
+        <div>
+            <div className="container">
+                <input type="text" placeholder="Search..."></input>
+                    <div className="search"></div>
+            </div>
+        </div>
+    )
+}
+
+export default SearchBar
